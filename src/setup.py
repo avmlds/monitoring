f7d8542a162051d8270c92bce11fa3f2fef892ff
alexander <64779548+avmlds@users.noreply.github.com>
2024-04-22 18:42:43 +0200

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="monitoring",
+    version="0.0.1",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "httpx >= 0.27.0",
+        "pydantic >= 2.7.0",
+        "prettytable >= 3.10.0",
+        "click >= 8.1.7",
+        "asyncpg >= 0.29.0",
+    ],
+    scripts=["cli/monitor"],
+    python_requires=">=3.7",
+)
