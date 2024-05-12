a65eb3343b99821325583fddccd4e92d96488146
alexander <64779548+avmlds@users.noreply.github.com>
2024-05-12 21:15:24 +0200

    Modify execution pipeline

--- src/monitoring/execution.py
+++ src/monitoring/execution.py
@@ -1,28 +1,81 @@
 import asyncio
+import copy
+import heapq
+import logging
+import multiprocessing
+from typing import List, Tuple
 
 from monitoring.config import Config, StartupConfiguration
-from monitoring.database import LocalDatabaseManager
-from monitoring.exceptions import NoServicesSpecifiedError
-from monitoring.workers import Agent, Collector, Exporter
+from monitoring.exceptions import NoServicesSpecifiedError, NotEnoughWorkersError
+from monitoring.models import HealthcheckConfig, ServiceResponse
+from monitoring.repositories import PostgresRepository
+from monitoring.task_manager import Agent, Exporter
+
+LOG = logging.getLogger()
 
 
-async def start_workers(
-    config: Config,
-    manager: LocalDatabaseManager,
-) -> None:
+def estimate_workload(config: Config) -> Tuple[float, float]:
+    """Estimate workload in RPM (requests per minute)."""
+
+    rpm = [1 / service.interval_sec for service in config.services]
+    total_rps = sum(rpm)
+    avg = total_rps / len(config.services)
+    LOG.warning(f"Average requests per second (RPS) across all services: {avg:.2f}")
+    LOG.warning(f"Total requests per second (RPS) across all services: {total_rps:.2f}")
+    return total_rps, avg
+
+
+async def start_workers(services: List[HealthcheckConfig], external_database_uri: str) -> None:
     """Start all the application workers."""
-    monitoring = Agent(config, manager)
-    exporter = Exporter(config, manager)
-    collector = Collector(config, manager)
-    await asyncio.gather(monitoring.start(), collector.start(), exporter.start())
+
+    q_size = len(services)
+
+    task_queue = copy.deepcopy(services)
+    heapq.heapify(task_queue)
+    result_queue: asyncio.Queue[ServiceResponse] = asyncio.Queue(maxsize=q_size)
+
+    monitoring = Agent()
+    exporter = Exporter()
+
+    database = PostgresRepository(external_database_uri)
+    await asyncio.gather(monitoring.start(task_queue, result_queue), exporter.start(database, result_queue))
+
+
+def _start(services: List[HealthcheckConfig], external_database_uri: str) -> None:
+    asyncio.run(start_workers(services, external_database_uri))
 
 
 def start(startup_configuration: StartupConfiguration) -> None:
     """Application's entrypoint."""
+
     config = Config.load(startup_configuration.config_path)
     if config.contains_no_service:
         raise NoServicesSpecifiedError
-    with LocalDatabaseManager.connect(config.local_database_path) as manager:
-        manager.init()
+
+    estimate_workload(config)
+
+    max_workers = startup_configuration.max_workers
+    service_chunks = [
+        config.services[i : i + startup_configuration.services_per_worker]
+        for i in range(0, len(config.services), startup_configuration.services_per_worker)
+    ]
+    if len(service_chunks) != max_workers:
+        raise NotEnoughWorkersError(
+            max_workers,
+            startup_configuration.services_per_worker,
+            len(service_chunks),
+        )
+
+    if max_workers == 1:
         startup_configuration.notify_systemd()
-        asyncio.run(start_workers(config, manager))
+        _start(config.services, config.external_database_uri)
+    else:
+        processes = []
+        for n in range(max_workers):
+            process = multiprocessing.Process(target=_start, args=(service_chunks[n], config.external_database_uri))
+            process.start()
+            processes.append(process)
+
+        startup_configuration.notify_systemd()
+        for p in processes:
+            p.join()
